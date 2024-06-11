c40cc152b69a7c362140d1a1b0c592563c83ace9
SahilMohite <109684495+SahilMohite@users.noreply.github.com>
2024-06-11 18:32:34 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,4 @@
+document.getElementById("redirectButton").addEventListener("click", function() {
+    // Replace 'destination.html' with the actual destination URL
+    window.location.href = "con.html";
+});
